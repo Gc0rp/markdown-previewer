510970bef8abbd4d42a0b15c8ab74c81e5978da5
Gc0rp <ojasgupta4321@gmail.com>
2019-07-18 09:35:34 +1000

    Updated user full screen feature.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,87 +1,83 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const ScreenTile = styled.div `
-    text-align: right;
-    // outline: 1px solid red;
-`;
+// import React from 'react';
+// import styled from 'styled-components';
 
 
-class Header extends React.Component {
 
-    constructor(props){
-        super(props);
+// class Header extends React.Component {
 
-        this.state = {
-            clicked: false
-        };
+//     constructor(props){
+//         super(props);
 
-        this.toggleFullScreen = this.toggleFullScreen.bind(this);
-    }
+//         this.state = {
+//             clicked: false
+//         };
 
-    toggleFullScreen(){
-        this.setState({
-            clicked: !this.state.clicked
-        });
+//         this.toggleFullScreen = this.toggleFullScreen.bind(this);
+//     }
 
-        console.log(this.state.clicked);
-    }
+//     toggleFullScreen(){
+//         this.setState({
+//             clicked: !this.state.clicked
+//         });
 
-    render() {
+//         console.log(this.state.clicked);
+//     }
 
-        return(
-            <div className="row">
-                <div className={this.state.clicked ? "col-lg-12" : "col-lg-6"}>
-                    <div className="row">
-                        <div className="col-lg-6" style={{display: "flex"}}>
-                            <i className="material-icons">edit</i> 
-                            <p>Editor</p> 
-                        </div>
+//     render() {
+
+//         return(
+//             <div className="row">
+//                 <div className={this.state.clicked ? "col-lg-12" : "col-lg-6"}>
+//                     <div className="row">
+//                         <div className="col-lg-6" style={{display: "flex"}}>
+//                             <i className="material-icons">edit</i> 
+//                             <p>Editor</p> 
+//                         </div>
                             
-                        <ScreenTile className="col-lg-6">
-                            <button type="button" className="btn btn-default" 
-                                onClick={this.toggleFullScreen}>
-                                <i className="material-icons"> fullscreen </i>
-                            </button>
-                        </ScreenTile>
-                    </div>
-                </div>
-            </div>
-        );   
-    }
-}
+//                         <ScreenTile className="col-lg-6">
+//                             <button type="button" className="btn btn-default" 
+//                                 onClick={this.toggleFullScreen}>
+//                                 <i className="material-icons"> fullscreen </i>
+//                             </button>
+//                         </ScreenTile>
+//                     </div>
+//                 </div>
+//             </div>
+//         );   
+//     }
+// }
 
-{/* <div className="row">
-                <div className="col-lg-6">
-                    <div className="row">
-                        <div className="col-lg-6" style={{display: "flex"}}>
-                            <i className="material-icons">edit</i> 
-                            <p>Editor</p> 
-                        </div>
+// {/* <div className="row">
+//                 <div className="col-lg-6">
+//                     <div className="row">
+//                         <div className="col-lg-6" style={{display: "flex"}}>
+//                             <i className="material-icons">edit</i> 
+//                             <p>Editor</p> 
+//                         </div>
 
                                               
-                    </div>
+//                     </div>
 
-                </div>
+//                 </div>
 
-                <div className="col-lg-6">
-                    <div className="row">
+                // <div className="col-lg-6">
+                //     <div className="row">
                     
-                        <div className="col-lg-6" style={{display: "flex"}}>
-                            <p>Previewer</p>
-                            <i className="material-icons">computer</i>
-                        </div>
+                //         <div className="col-lg-6" style={{display: "flex"}}>
+                //             <p>Previewer</p>
+                //             <i className="material-icons">computer</i>
+                //         </div>
 
-                        <ScreenTile className="col-lg-6">
-                            <button type="button" className="btn btn-default">
-                                <i className="material-icons"> fullscreen </i>
-                            </button>
-                        </ScreenTile> 
+                //         <ScreenTile className="col-lg-6">
+                //             <button type="button" className="btn btn-default">
+                //                 <i className="material-icons"> fullscreen </i>
+                //             </button>
+                //         </ScreenTile> 
                     
-                    </div>
+                //     </div>
                 
-                </div>
+//                 </div>
                 
-</div> */}
+// </div> */}
 
-export default Header;
+// export default Header;
--- src/components/MarkdownPreview.js
+++ src/components/MarkdownPreview.js
@@ -2,8 +2,6 @@ import React, {Fragment} from 'react';
 import styled from 'styled-components';
 import marked from 'marked';
 
-import Header from './Header';
-
 const UserText = styled.textarea `
     background-color: #473144;
     color: white;
@@ -24,16 +22,27 @@ const GeneratedOutput = styled.div `
     margin: 15px auto;
 `;
 
+const ScreenTile = styled.div `
+    text-align: right;
+    // outline: 1px solid red;
+`;
+
 class MarkdownPreview extends React.Component {
 
     constructor(props){
         super(props);
 
         this.state = {
-            text : ''
+            text : '',
+            userFullScreen : false,
+            displayRender: "visible",
+            renderFullScreen: false,
+            displayUserInput: true
         };
 
         this.textChange= this.textChange.bind(this);
+        this.toggleUserFullScreen = this.toggleUserFullScreen.bind(this);
+        // this.toggleRenderFullScreen = this.toggleRenderFullScreen.bind(this);
     }
 
     textChange(event){
@@ -43,23 +52,67 @@ class MarkdownPreview extends React.Component {
         });        
     }
 
+    toggleUserFullScreen(){
+        this.setState({
+            userFullScreen: !this.state.userFullScreen
+        });
+
+        if(this.state.displayRender === "visible") {
+            this.state.displayRender = "hidden";
+        } else {
+            this.state.displayRender = "visible";
+        }
+
+    }
+
     render() {
         const rt = marked(this.state.text);
         return(
             <Fragment>
-                <Header />
-            <div className="row">
-                <div className="col-lg-6">  
-                    <UserText onChange={this.textChange} value={this.state.text}/>
-                </div>
+                <div className="row">
+                    <div className={this.state.userFullScreen ? "col-lg-12" : "col-lg-6"}>
+                        <div className="row">
+                            <div className="col-lg-6" style={{display: "flex"}}>
+                                <i className="material-icons">edit</i> 
+                                <p>Editor</p> 
+                            </div>
+                            
+                            <ScreenTile className="col-lg-6">
+                                <button type="button" className="btn btn-default" 
+                                    onClick={this.toggleUserFullScreen}>
+                                    <i className="material-icons"> fullscreen </i>
+                                </button>
+                            </ScreenTile>
+                        </div>
+                    </div>
 
-                <div className="col-lg-6">
+                    <div className={this.state.renderFullScreen ? "col-lg-12" : "col-lg-6"} 
+                    style={{visibility: this.state.displayRender}}>
+                        <div className="row">
+                            <div className="col-lg-6" style={{display: "flex"}}>
+                                <p>Previewer</p>
+                                <i className="material-icons">computer</i>
+                            </div>
+                            <ScreenTile className="col-lg-6">
+                                <button type="button" className="btn btn-default">
+                                    <i className="material-icons"> fullscreen </i>
+                                </button>
+                            </ScreenTile> 
+                        </div>
                     
-                    <GeneratedOutput dangerouslySetInnerHTML={{ __html: rt }} />
-
+                    </div>
                 </div>
 
-            </div>
+                <div className="row">
+                    <div className={this.state.userFullScreen ? "col-lg-12" : "col-lg-6"}>  
+                        <UserText onChange={this.textChange} value={this.state.text}/>
+                    </div>
+                    
+                    <div className="col-lg-6">
+                        <GeneratedOutput dangerouslySetInnerHTML={{ __html: rt }} 
+                            style={{visibility: this.state.displayRender}}/>
+                    </div>
+                </div>
             </Fragment>
             
         );
